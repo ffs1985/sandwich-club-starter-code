0af6801cb4afbb8bc81b2e12885da64e57747351
Federico Fontes <federico.fontes@globant.com>
2018-04-29 22:07:10 -0700

    Sandwich Club Project - Edit JsonUtils to parse json file correctly. - Modify activity_detail layout to support scroll and present properly. - Edit DetailActivity.java to populate the UI correctly.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,42 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(json);
+            JSONObject name = (JSONObject)jsonObject.get("name");
+            sandwich = new Sandwich();
+            sandwich.setMainName((String)name.get("mainName"));
+            JSONArray alsoKnownAs = (JSONArray)name.get("alsoKnownAs");
+            List<String> alsoKnowAsList = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAs.length(); i++) {
+                alsoKnowAsList.add((String)alsoKnownAs.get(i));
+            }
+            sandwich.setAlsoKnownAs(alsoKnowAsList);
+            sandwich.setPlaceOfOrigin((String)jsonObject.get("placeOfOrigin"));
+            sandwich.setDescription((String)jsonObject.get("description"));
+            sandwich.setImage((String)jsonObject.get("image"));
+            JSONArray ingredients = (JSONArray)jsonObject.get("ingredients");
+            List<String> ingredientList = new ArrayList<>();
+            for (int i = 0; i < ingredients.length(); i++) {
+                ingredientList.add((String)ingredients.get(i));
+            }
+            sandwich.setIngredients(ingredientList);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
